fa6657bfa54fbe62d2ff4d97cbad4fd3e87e72ab
Vitor Marcelo <vitor.marcelo2009@hotmail.com>
2023-08-22 16:03:00 -0300

    Feat: Iniciando Projeto

---
+++ Aulas/aula-01/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function PageNotFound() {
+  return (
+    <>
+      <main>
+        <div>
+          <h1>404 - Página não encontrada.</h1>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ Aulas/projeto-pokenext/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>Iniciando Projeto</h1>
+      </div>
+    </main>
+  );
+}
